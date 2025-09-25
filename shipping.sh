23505a42a24e2de1df0d272a6d9d25c3d263a433
rakesh <rakesh@gmail.com>
2025-09-25 17:04:38 +0530

    Change

--- shipping.sh
+++ shipping.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MYSQL_HOST=$(mysql.expense.icu)
+MYSQL_HOST=mysql.expense.icu
 START_TIME=$(date +%s)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
